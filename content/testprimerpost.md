d86c48f3e45ce4d2e89d338652434bbec294cb0d
Sergi <you@example.com>
2013-02-27 01:03:29 +0100

    init

---
+++ content/testprimerpost.md
@@ -0,0 +1,8 @@
+Title: hello world, nuevo Blog estático
+date: 28-02-2013
+Tags: pelican, hola
+Category: personal
+Slug: my-super-post
+Summary: Aquí comienzo el primer post de mi nuevo blog estático, hecho en Pelican
+
+Mi nuevo blog hecho en Pelican, sin bases de datos, solo yo, MarDown y Python... entre buenos amigos.
---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Sergi Collado'
+SITENAME = u'Sergi Collado'
+SITEURL = 'sergicollado.com'
+FEED_DOMAIN = "http://www.sergicollado.com"
+THEME = "waterspill"
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'es'
+
+# Blogroll
+LINKS =  (('Becode', 'http://becodemyfriend.com'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('tw1ttw3r', 'https://twitter.com/circun4'),('G00gl3+', 'https://plus.google.com/u/0/108686854890472733391/posts/p/pub'))
+
+DEFAULT_PAGINATION = 10
